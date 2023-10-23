1cdee2336d9fff40867606b179bcaf12fd263f86
Burakkey <Burak.unat@mail.utoronto.ca>
2023-10-23 13:42:11 -0400

    Basic

---
+++ src/entity/InstagramStats.java
@@ -0,0 +1,4 @@
+package entity;
+
+public class InstagramStats implements SocialMediaStats{
+}
---
+++ src/entity/User.java
@@ -0,0 +1,19 @@
+package entity;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class User {
+    private String username;
+    private String password;
+    private List<Integer> data;
+
+    public void User(String username, String password){
+        this.username = username;
+        this.password = password;
+
+
+
+    }
+
+}
