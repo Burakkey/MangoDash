ffe300622824f97bc2fe6675d45f921f052539c9
Ariel Liu <44077856+arielycliu@users.noreply.github.com>
2023-11-27 12:30:22 -0500

    Created FacebookStats Class
    - added stats attribute

--- src/main/java/entity/SocialMediaStats/FacebookStats.java
+++ src/main/java/entity/SocialMediaStats/FacebookStats.java
@@ -1,1 +1,16 @@
 package entity.SocialMediaStats;
+
+import org.json.JSONArray;
+import java.util.HashMap;
+
+public class FacebookStats implements SocialMediaStats {
+    private String apiKey;
+    private HashMap<String, JSONArray> stats;
+
+    public FacebookStats() {
+        stats = new HashMap<>();
+        stats.put("friends", new JSONArray().put(0));
+        stats.put("posts", new JSONArray());
+        stats.put("posts_data", new JSONArray());
+    }
+}
