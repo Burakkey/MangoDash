0c872f9b06cbf7e74b01dbee0344fc453a6ca6e3
Cindolinna <cindolinna.wong@mail.utoronto.ca>
2023-11-27 14:36:55 -0500

    implemented the fail view for when the Name contains chars other than letters

--- src/interface_adapter/signup/SignupState.java
+++ src/interface_adapter/signup/SignupState.java
@@ -2,10 +2,8 @@ package interface_adapter.signup;
 
 public class SignupState {
     private String name = "";
-
-    private String nameError = "";
     private String username = "";
-    private String usernameError = null;
+    private String error = null;
     private String password = "";
     private String passwordError = null;
     private String repeatPassword = "";
@@ -14,7 +12,7 @@ public class SignupState {
     public SignupState(SignupState copy) {
         name = copy.name;
         username = copy.username;
-        usernameError = copy.usernameError;
+        error = copy.error;
         password = copy.password;
         passwordError = copy.passwordError;
         repeatPassword = copy.repeatPassword;
@@ -29,63 +27,41 @@ public class SignupState {
         return name;
     }
 
-    public String getNameError(){
-        return nameError;
-    }
-
     public String getUsername() {
         return username;
     }
 
-    public String getUsernameError() {
-        return usernameError;
+    public String getError() {
+        return error;
     }
 
     public String getPassword() {
         return password;
     }
 
-    public String getPasswordError() {
-        return passwordError;
-    }
-
     public String getRepeatPassword() {
         return repeatPassword;
-    }
-
-    public String getRepeatPasswordError() {
-        return repeatPasswordError;
     }
 
     public void setName(String name){
         this.name = name;
     }
 
-    public void setNameError(String name){
-        this.name = name;
-    }
     public void setUsername(String username) {
         this.username = username;
     }
 
-    public void setUsernameError(String usernameError) {
-        this.usernameError = usernameError;
+    public void setError(String usernameError) {
+        this.error = usernameError;
     }
 
     public void setPassword(String password) {
         this.password = password;
     }
 
-    public void setPasswordError(String passwordError) {
-        this.passwordError = passwordError;
-    }
 
     public void setRepeatPassword(String repeatPassword) {
         this.repeatPassword = repeatPassword;
-    }
-
-    public void setRepeatPasswordError(String repeatPasswordError) {
-        this.repeatPasswordError = repeatPasswordError;
     }
 
     @Override
