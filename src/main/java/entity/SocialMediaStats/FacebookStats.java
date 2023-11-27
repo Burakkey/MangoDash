c029425976b390d5cff6dc75d46463742e115539
Ariel Liu <44077856+arielycliu@users.noreply.github.com>
2023-11-27 12:39:14 -0500

    Created getPostData to parse post likes

--- src/main/java/entity/SocialMediaStats/FacebookStats.java
+++ src/main/java/entity/SocialMediaStats/FacebookStats.java
@@ -26,6 +26,7 @@ public class FacebookStats implements SocialMediaStats {
         FacebookStats facebook = new FacebookStats();
         String key = "EAAE1wmc7D2IBOzhnp8Y4GlGZACmqvPmXaCI6WzLwctdNYpwp0uYE3XmgGFKVe3rhwBa0DNDZCwxJOcZCJOqg9h53L4MwRO0UcmxFEZBxw3YqOmfL4kLxFWO3ZBO6ltLClQrLtDkCuMjWZB8weZAAnorf0axciB3YXaZCgsdtD4ROSDU6IKhCzEd11iaZCUZCtROwSJ09f55Wv0U7TErW0gzLaXGMbNsXgtEfV7dPnXNitQor0LCi3awhmHN2WyPK5HzooZD";
         facebook.setApiKey(key);
+        facebook.getPostData();
         System.out.println(facebook.stats);
     }
 
@@ -42,6 +43,31 @@ public class FacebookStats implements SocialMediaStats {
             updateStats();
         } catch (MalformedURLException exception) {
             System.out.println("API call failed, possible expired token");
+        }
+    }
+
+    public void getPostData() throws MalformedURLException {
+        JSONArray posts = stats.get("posts");
+
+        for (int i = 0; i < posts.length(); i++) {
+            JSONObject post = posts.getJSONObject(i);
+            String postID = post.getString("id");
+//            System.out.println();
+//            System.out.println(postID);
+            URL urlGetPostLikes = new URL(
+                    "https://graph.facebook.com/v18.0/" + postID + "?fields=reactions.summary(1),comments.summary(1)&access_token="
+                            + apiKey);
+
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(urlGetPostLikes.openStream(), "UTF-8"))) {
+                for (String line; (line = reader.readLine()) != null;) {
+                    JSONObject object = new JSONObject(line);
+                    JSONObject postReactions = (JSONObject) object.get("reactions");
+                    JSONObject postSummaryReactions = (JSONObject) postReactions.get("summary");
+//                    System.out.println(postSummaryReactions);
+                }
+            } catch (IOException e) {
+                System.out.println("Error with API call to get user posts summary");
+            }
         }
     }
 
