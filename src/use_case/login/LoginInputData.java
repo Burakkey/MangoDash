2619cbb92bf6897379114114da99859342780be4
Cindolinna <cindolinna.wong@mail.utoronto.ca>
2023-11-28 13:55:41 -0500

    LoginInputData javadocs for public class+methods

--- src/use_case/login/LoginInputData.java
+++ src/use_case/login/LoginInputData.java
@@ -1,10 +1,18 @@
 package use_case.login;
 
+/**
+ * LoginInputData contains the required data for a user to sign up.
+ */
 public class LoginInputData {
 
     final private String username;
     final private String password;
 
+    /**
+     * Creates a new LoginInputData object.
+     * @param username the user's username, from the Username text field
+     * @param password the user's corresponding password, from the Password text field
+     */
     public LoginInputData(String username, String password) {
         this.username = username;
         this.password = password;
