5d9762b70d975f0817a7b7d1f99278ce44ec5bdc
Cindolinna <cindolinna.wong@mail.utoronto.ca>
2023-11-28 13:44:45 -0500

    LoginViewModel javadocs for public classes+methods

--- src/interface_adapter/login/LoginViewModel.java
+++ src/interface_adapter/login/LoginViewModel.java
@@ -6,6 +6,9 @@ import java.awt.*;
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
 
+/**
+ * LoginViewModel is a data structure that contains all the data that the LoginView needs.
+ */
 public class LoginViewModel extends ViewModel {
 
     public final String TITLE_LABEL = "Log In View";
@@ -19,6 +22,9 @@ public class LoginViewModel extends ViewModel {
 
     private LoginState state = new LoginState();
 
+    /**
+     * Creates a new LoginViewModel. There are no parameters, as all the instance variables are predetermined.
+     */
     public LoginViewModel() {
         super("log in");
     }
@@ -29,8 +35,9 @@ public class LoginViewModel extends ViewModel {
 
     private final PropertyChangeSupport support = new PropertyChangeSupport(this);
 
-    // This is what the Signup Presenter will call to let the ViewModel know
-    // to alert the View
+    /**
+     * the LoginPresenter calls firePropertyChanged to let the LoginViewModel know to alert the LoginView that a property has been changed
+     */
     public void firePropertyChanged() {
         support.firePropertyChange("state", null, this.state);
     }
