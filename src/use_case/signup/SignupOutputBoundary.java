e65c0bfe87566c6e5df86a63c4d8f962b7bf23b5
Cindolinna <cindolinna.wong@mail.utoronto.ca>
2023-11-28 13:48:44 -0500

    SignupOutputBoundary - fixed a javadoc

--- src/use_case/signup/SignupOutputBoundary.java
+++ src/use_case/signup/SignupOutputBoundary.java
@@ -1,7 +1,7 @@
 package use_case.signup;
 
 /**
- * SignupOutputBoundary is an interface with methods that detail how the program will respond to
+ * SignupOutputBoundary is an interface with methods that detail how the program will respond to the input data
  */
 public interface SignupOutputBoundary {
     /**
