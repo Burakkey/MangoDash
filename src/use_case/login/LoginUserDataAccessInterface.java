5bf07847c2b9e4d50cc9e2093d0c4f3e816028ad
Cindolinna <cindolinna.wong@mail.utoronto.ca>
2023-11-28 14:05:45 -0500

    LoginUserDataAccessInterface javadocs for public class+methods; deleted the save method since logging in doesn't save anything

--- src/use_case/login/LoginUserDataAccessInterface.java
+++ src/use_case/login/LoginUserDataAccessInterface.java
@@ -2,10 +2,17 @@ package use_case.login;
 
 import entity.User;
 
+/**
+ * LoginUserDataAccessInterface is an interface that details all the methods that require access to data objects
+ * during the login process.
+ */
 public interface LoginUserDataAccessInterface {
+    /**
+     * existsByName takes the given String and checks the database to see if that String exists in the database as an existing Username
+     * @param identifier the username the user has inputted into login Username text field
+     * @return true if the username is in the database, false otherwise
+     */
     boolean existsByName(String identifier);
-
-    void save(User user);
 
     User get(String username);
 }
