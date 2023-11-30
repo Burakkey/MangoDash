25b8f778f7d7e7296dee1ae5ffbaad276b38817a
Cindolinna <cindolinna.wong@mail.utoronto.ca>
2023-11-30 17:08:13 -0500

    LoginOutputData javadocs for public class+methods - descriptions for facebookAPI and instagramAPI params not filled in

--- src/use_case/login/LoginOutputData.java
+++ src/use_case/login/LoginOutputData.java
@@ -1,5 +1,8 @@
 package use_case.login;
 
+/**
+ * LoginOutputData contains the required data after the user has logged in
+ */
 public class LoginOutputData {
 
     private final String username;
@@ -13,6 +16,15 @@ public class LoginOutputData {
     private final String instagramAPI;
     private boolean useCaseFailed;
 
+    /**
+     * Creates a new LoginOutputData object.
+     * @param name the name the user specified during sign up
+     * @param username the user's username
+     * @param bio the user's chosen biography, if any
+     * @param facebookAPI
+     * @param instagramAPI
+     * @param useCaseFailed
+     */
     public LoginOutputData(String name, String username, String bio, String facebookAPI, String instagramAPI, boolean useCaseFailed) {
         this.name = name;
         this.username = username;
