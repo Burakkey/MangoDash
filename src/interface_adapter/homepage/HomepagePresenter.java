a3ad8df168a74d35c0c6ff9f9cb54a224a19e342
Ariel Liu <44077856+arielycliu@users.noreply.github.com>
2023-12-03 11:37:35 -0500

    Within HomepagePresenter created facebookStatsHashMap()

--- src/interface_adapter/homepage/HomepagePresenter.java
+++ src/interface_adapter/homepage/HomepagePresenter.java
@@ -1,9 +1,11 @@
 package interface_adapter.homepage;
 
+import entity.SocialMediaStats.FacebookStats;
 import interface_adapter.ViewManagerModel;
 import interface_adapter.login.LoginViewModel;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import use_case.FacebookDataGetter;
 import use_case.InstagramDataGetter;
 import use_case.change_user_data.ChangeDataOutput;
 import use_case.change_user_data.ChangeDataOutputBoundary;
@@ -54,6 +56,64 @@ public class HomepagePresenter implements ChangeDataOutputBoundary {
         homepageState.setInstagramStatsHashMap(instagramStatsHashMap);
         this.homepageViewModel.firePropertyChanged();
         this.viewManagerModel.firePropertyChanged();
+    }
+
+    public static HashMap<String, Object> makeFacebookStatsHashmap(FacebookDataGetter changeDataOutput) {
+        JSONArray arrayFollowers = (JSONArray) changeDataOutput.getFacebookData().get("followers");
+        JSONArray arrayPosts = (JSONArray) changeDataOutput.getFacebookData().get("posts");
+        JSONArray arrayUsername = (JSONArray) changeDataOutput.getFacebookData().get("username");
+        System.out.println(changeDataOutput.getFacebookData());
+
+        // Assume the first element of arrayFollowers is the total friend count
+        int followersCount = !arrayFollowers.isEmpty() ? arrayFollowers.getInt(0) : 0;
+        int maxLikes = 0;
+        int maxComments = 0;
+        int totalLikes = 0;
+        int totalComments = 0;
+
+        String username = null;
+        if (arrayUsername != null && !arrayUsername.isEmpty()) {
+            username = (String) arrayUsername.get(0);
+        }
+        List<Integer> likesPerPost = new ArrayList<>();
+        List<Integer> commentsPerPost = new ArrayList<>();
+
+        for (int i = 0; i < arrayPosts.length(); i++) {
+            JSONObject post = arrayPosts.getJSONObject(i);
+            int likes = post.getInt("like_count");
+            int comments = post.getInt("comments_count");
+
+            if (likes > maxLikes) {
+                maxLikes = likes;
+            }
+            likesPerPost.add(likes);
+            commentsPerPost.add(comments);
+            if (comments > maxComments) {
+                maxComments = comments;
+            }
+            totalLikes += likes;
+            totalComments += comments;
+        }
+
+        int totalPosts = arrayPosts.length();
+        double averageLikes = totalPosts > 0 ? (double) totalLikes / totalPosts : 0;
+        double averageComments = totalPosts > 0 ? (double) totalComments / totalPosts : 0;
+
+        HashMap<String, Object> facebookStatsHashMap = new HashMap<>();
+
+        // Add the calculated statistics to the hashmap
+        facebookStatsHashMap.put("followersCount", followersCount);
+        facebookStatsHashMap.put("maxLikes", maxLikes);
+        facebookStatsHashMap.put("maxComments", maxComments);
+        facebookStatsHashMap.put("totalLikes", totalLikes);
+        facebookStatsHashMap.put("totalComments", totalComments);
+        facebookStatsHashMap.put("averageLikes", averageLikes);
+        facebookStatsHashMap.put("averageComments", averageComments);
+        facebookStatsHashMap.put("likesPerPost", likesPerPost);
+        facebookStatsHashMap.put("commentsPerPost", commentsPerPost);
+        facebookStatsHashMap.put("totalPosts", totalPosts);
+        facebookStatsHashMap.put("username", username);
+        return facebookStatsHashMap;
     }
 
     public static HashMap<String, Object> makeInstagramStatsHashmap(InstagramDataGetter changeDataOutput) {
