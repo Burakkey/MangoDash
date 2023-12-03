4b9af90cb0a5cfc291ea893c1b36d3d95c66e04a
Ariel Liu <44077856+arielycliu@users.noreply.github.com>
2023-12-03 09:58:51 -0500

    Added function isApiError in FacebookAPIDAInterface

--- src/use_case/change_user_data/FacebookAPIDataAccessInterface.java
+++ src/use_case/change_user_data/FacebookAPIDataAccessInterface.java
@@ -7,5 +7,6 @@ import java.net.MalformedURLException;
 public interface FacebookAPIDataAccessInterface {
     void fetchData() throws MalformedURLException;
     void setApi(String apiKey);
+    boolean isApiError();
     FacebookStats getFacebookStats();
 }
