82dee58227b60c38193ec5fd97a5869bf5325e69
aungzm06 <hisham.maw@mail.utoronto.ca>
2023-12-04 20:41:56 -0500

    uploading testusers.db for testing purposes

--- src/view/homepage/ExtensionPanelComponents.java
+++ src/view/homepage/ExtensionPanelComponents.java
@@ -10,8 +10,6 @@ import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
